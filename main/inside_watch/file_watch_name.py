9365f504481e7a97d8a75aa1953646167598fec6
ImportEverythingAsk <80804905+ImportEverythingAsk@users.noreply.github.com>
2021-05-08 20:38:09 -0500

    Github didn

--- main/inside_watch/file_watch_name.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-def make_name_label(watch_frame, watch_name):
-    inside_label_frame_for_label = LabelFrame(watch_frame)
-    inside_label_frame_for_label.grid(row=0, column=0)
-    # Allow option to change watch's name.
-    inside = Label(inside_label_frame_for_label, text=watch_name)
-    inside.grid(row=0, column=0)
